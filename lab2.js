66b4f73bf27d3644c1225a7dd64d4495f91ba966
razanalnanaah <razanalnanaah@yahoo.com>
2019-10-07 16:17:30 +0300

    why

---
+++ lab2.js
@@ -0,0 +1,92 @@
+'use strlct';
+var user = prompt(" Can You Please Tell Us Your Name ")
+alert("Welcome You Here " + user + " I wish You Enjoy Knowing Me..")
+var name= prompt('Do you know what my name is??');
+
+name = name.toLowerCase();
+if (name =='yes'){
+
+
+    alert(" WOW!! You made me happy");
+    console.log ("WOW!! You made me happy");
+    }else if (name =='y'){
+        alert(" WOW!! You made me happy");
+        console.log ("WOW!! You made me happy");
+    }
+    else if (name =='no'){
+    alert("Saddened me !! Now you know me");
+    console.log ("Saddened me !! Now you know me");
+    }
+    
+    var study= prompt('Have an idea about what I studied at the university?');
+
+study = study.toLowerCase();
+if (study =='yes'){
+
+
+    alert("GREAT^_^");
+    console.log ("GREAT^_^");
+    }else if (study =='y'){
+        alert("GREAT^_^ ");
+        console.log ("GREAT^_^");
+    }
+    else if (study =='no'){
+    alert("It's okay !!");
+    console.log ("It's okay !!");
+    }
+    
+    var age= prompt('What about my age do you know??');
+
+    age= age.toLowerCase();
+    if (age =='yes'){
+    
+    
+        alert(" hahah Nice  ");
+        console.log ("hahah Nice ");
+        }else if (age =='y'){
+            alert(" hahah Nice ");
+            console.log ("hahah Nice ");
+        }
+        else if (age =='no'){
+        alert("never mind");
+        console.log ("never mind");
+        }
+
+        var color= prompt('Do you know my favorite color??');
+
+    color= color.toLowerCase();
+    if (color =='yes'){
+    
+    
+        alert(" Iam so happy haha ");
+        console.log (" Iam so happy haha");
+        }else if (color =='y'){
+            alert(" Iam so happy haha ");
+            console.log (" Iam so happy haha");
+        }
+        else if (color =='no'){
+        alert("okay");
+        console.log ("okay");
+        }
+
+        
+        var questions= prompt('Are the questions boring??');
+
+    questions= questions.toLowerCase();
+    if (questions =='yes'){
+    
+    
+        alert(" Iam so SORRY ");
+        console.log ("Iam so SORRY");
+        }else if (questions =='y'){
+            alert(" Iam so SORRY ");
+            console.log ("Iam so SORRY");
+        }
+        else if (questions =='no'){
+        alert("THANK YOU ");
+        console.log ("THANK YOU");
+        }
+
+        alert("Thanks for being here " + user);
+        
+
